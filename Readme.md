40d365bce6f4e4b96daff2ca1de6fb2611621bdc
ParikshitShetty <parikshitshetty66@gmail.com>
2024-10-16 22:22:31 +0530

    initial commit

---
+++ Readme.md
@@ -0,0 +1,18 @@
+To run the backend
+
+### Actiavte venv environment
+```
+.\venv\Scripts\activate
+```
+
+### To run the project
+
+- For Production
+```
+fastapi run
+```
+
+- For Development
+```
+fastapi dev
+```
---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='min-h-screen w-full bg-black text-white'>
+        Library
+      </div>
+    </>
+  )
+}
+
+export default App
